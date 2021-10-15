44b87d48eb9b8a864f45d6df46fa182236dccb91
lusqua <lucasaguiar.floripa@gmail.com>
2021-10-15 19:10:55 -0300

    freeze_suport added

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 from PySimpleGUI import PySimpleGUI as sg
-from multiprocessing import Process
- 
+from multiprocessing import Process, Manager, freeze_support
+
 from classes.action_functions import Action_functions, Actual_signal, Where_save
 from classes.process import Process as Operate
 from classes.layouts import Layouts
 from classes.readTXT import Read
 from classes.api_main import Api
- 
+
 
 class Main():
- 
+
     def __init__(self):
 
         operate = False
         api = Api()
-        window_astroend, window_file, window_login, pop_up, window_option, window_trading, window_terminate = Layouts.window_astroend(), None, None, None, None, None, None
-        
+        window_astroend, window_file, window_login, pop_up, window_option, window_trading, window_terminate = Layouts.window_astroend(
+        ), None, None, None, None, None, None
+
         status, message = Where_save().find()
 
         if status:
             file_historic = message
 
         while True:
- 
+
             window, event, values = sg.read_all_windows(timeout=25)
- 
+
             if window == window_astroend and event == sg.WIN_CLOSED:
                 break
- 
+
             if window == window_astroend and event == 'TERMOS':
-                link = Process(target= Action_functions.open_link)
+                link = Process(target=Action_functions.open_link)
                 link.start()
-                    
+
             if window == window_astroend and event == 'ACEITO OS TERMOS':
                 window_astroend.close()
 
@@ -52,20 +53,20 @@ class Main():
                 status, message = Where_save().find()
                 if status:
                     file_historic = message
-                
+
                 window_file.close()
                 window_login = Layouts.window_login()
-            
+
             if window == window_login and event == sg.WIN_CLOSED:
                 break
- 
+
             if window == window_login and event == 'Entrar':
-                
-                window_login.close()   
- 
-                loading = Process(target  = Action_functions.gif)
+
+                window_login.close()
+
+                loading = Process(target=Action_functions.gif)
                 loading.start()
- 
+
                 if(api.connect(values['login'], values['password'])):
                     loading.terminate()
                     window_option = Layouts.window_option()
@@ -73,34 +74,38 @@ class Main():
                     loading.terminate()
                     pop_up = Layouts.pop_up()
                     window_login = Layouts.window_login()
-                
+
             if window == window_option and event == sg.WIN_CLOSED:
                 break
- 
+
             if window == window_option and event == 'Enviar':
                 window_option['listbox'].update(Read(values['file']).to_list())
                 file = Read(values['file']).convert()
-            
+
             if window == window_option and event == 'Operar':
- 
+
                 api.type('PRACTICE' if values['account_practice'] else 'REAL')
                 api.option('BINARY' if values['option_binary'] else 'DIGITAL')
-                api.value(float(((str(values['value'])).replace(',','.'))))
-                api.stop_win(float(((str(values['stop_win'])).replace(',','.'))))
-                api.stop_loss(float((str(values['stop_loss'])).replace(',','.')))
+                api.value(float(((str(values['value'])).replace(',', '.'))))
+                api.stop_win(
+                    float(((str(values['stop_win'])).replace(',', '.'))))
+                api.stop_loss(
+                    float((str(values['stop_loss'])).replace(',', '.')))
                 api.martingale(int(values['martingale']))
-                api.multiplicador(float((str(values['multiplier'])).replace(',','.')))
+                api.multiplicador(
+                    float((str(values['multiplier'])).replace(',', '.')))
 
-                trading_ = Process(target=Operate, args=(api.get_email, api.get_password, 
-                    api.get_type, api.get_value, api.get_martingale, file,
-                    api.get_option, api.get_stop_win, api.get_stop_loss, api.get_multiplicador))
- 
+                trading_ = Process(target=Operate, args=(api.get_email, api.get_password,
+                                                         api.get_type, api.get_value, api.get_martingale, file,
+                                                         api.get_option, api.get_stop_win, api.get_stop_loss, api.get_multiplicador))
+
                 trading_.start()
- 
+
                 window_option.close()
                 operate = True
-                window_trading = Layouts.window_trading(api.get_type, api.get_balance, api.get_martingale, api.get_stop_win_complete, api.get_stop_loss_complete)
-            
+                window_trading = Layouts.window_trading(
+                    api.get_type, api.get_balance, api.get_martingale, api.get_stop_win_complete, api.get_stop_loss_complete)
+
             if window == window_trading and event == sg.WIN_CLOSED:
                 trading_.terminate()
                 operate = False
@@ -108,8 +113,10 @@ class Main():
 
             if operate:
                 window_trading['-balance-'].update(f"Banca: {api.get_balance}")
-                window_trading['-status-'].update('Meta batida' if api.get_balance >= api.get_stop_win_complete else 'Stop atingido' if api.get_balance <= api.get_stop_loss_complete else f'Trabalhando ...' if api.get_balance > 0 else 'Sem fundos.')
-                window_trading['-signal-'].update(f"{Actual_signal(file).get_next()}")
+                window_trading['-status-'].update('Meta batida' if api.get_balance >= api.get_stop_win_complete else 'Stop atingido' if api.get_balance <=
+                                                  api.get_stop_loss_complete else f'Trabalhando ...' if api.get_balance > 0 else 'Sem fundos.')
+                window_trading['-signal-'].update(
+                    f"{Actual_signal(file).get_next()}")
 
             if window == window_trading and event == 'Finalizar':
                 trading_.terminate()
@@ -126,4 +133,5 @@ class Main():
 
 
 if __name__ == '__main__':
+    freeze_support()
     Main()
